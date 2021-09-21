28d193a8324ce5a22706b2c6ddfbcc328a713f5d
LIniquer Silva <liniquer.silva@vr.com.br>
2021-09-21 09:25:00 -0300

    incluir example env

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+const { DB_DIALECT, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } =
+  process.env;
 module.exports = {
   development: {
-    dialect: "postgres",
-    host: "localhost",
-    username: "default",
-    password: "secret",
-    database: "projeto3",
+    dialect: DB_DIALECT || "postgres",
+    host: DB_HOST || "localhost",
+    username: DB_USERNAME || "default",
+    password: DB_PASSWORD || "secret",
+    database: DB_DATABASE,
     define: {
       timestamps: true, //  para incluir o created_at e updated_ad
       underscored: true, // modelo snackcase
