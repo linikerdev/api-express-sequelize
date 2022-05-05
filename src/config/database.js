24604ca84c56aff59c0f675d277b38f909127b8a
Liniquer Silva <contato@liniker.com.br>
2022-05-05 00:34:01 -0300

    atualizando conexoes

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,4 @@
+// SQLITE 
 module.exports = {
     development: {
         dialect: 'sqlite',
@@ -5,3 +6,22 @@ module.exports = {
         dialectOptions: {}
     }
 }
+// POSTGRES SQL
+// require("dotenv").config();
+// const { DB_DIALECT, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } =
+//   process.env;
+// module.exports = {
+//   development: {
+//     dialect: DB_DIALECT || "postgres",
+//     host: DB_HOST || "localhost",
+//     username: DB_USERNAME || "default",
+//     password: DB_PASSWORD || "secret",
+//     database: DB_DATABASE,
+//     define: {
+//       timestamps: true, //  para incluir o created_at e updated_ad
+//       underscored: true, // modelo snackcase
+//     },
+//   },
+//   production: {},
+// };
+
